03d6d412ad175f895279d9383abb41afea87b8a8
Guilherme Marian <guimarian123@gmail.com>
2025-10-22 10:45:03 -0300

    commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = '';
+
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillReact(100, 50, 100, 100);
+    ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);  
+    requestAnimationFrame(animate);
+}
+animate();
