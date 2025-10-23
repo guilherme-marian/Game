be1f7e5c96e9d182646293a25d12a782eccb906e
Guilherme Marian <guimarian123@gmail.com>
2025-10-22 22:28:29 -0300

    sprites

--- script.js
+++ script.js
@@ -10,21 +10,62 @@ const spriteHeight = 32;
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
-
+const columns = 8;
 const renderScale = 10;
+const staggerFrames = 10;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 4,
+        row: 0
+    },
+    {
+        name: 'run',
+        frames: 16,
+        row: 2
+    },
+    {
+        name: 'roll',
+        frames: 8,
+        row: 5
+    },
+    {
+        name: 'hit',
+        frames: 4,
+        row: 6
+    },
+    {
+        name: 'death',
+        frames: 4,
+        row: 7
+    }
+];
 
+animationStates.forEach((state) => {
+    let frames = {
+        loc: []
+    };
+
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = (j % columns) * spriteWidth;
+        let positionY = (state.row + Math.floor(j / columns)) * spriteHeight;
+        frames.loc.push({ x: positionX, y: positionY });
+    }
+
+    spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    //ctx.fillRect(100, 50, 100, 100);
+    const animation = spriteAnimations['death'];
+    let position = Math.floor(gameFrame / staggerFrames) % animation.loc.length;
+    const frame = animation.loc[position];
     //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-    ctx.drawImage(playerImage, frameX * spriteWidth, 
-        frameY * spriteHeight, spriteWidth , spriteHeight, 0, 0,
-        spriteWidth * renderScale, spriteHeight * renderScale);  
-    if(gameFrame % 10 === 0){
-        if(frameX < 3) frameX++;
-        else frameX = 0;
-    }
+    ctx.drawImage(playerImage, frame.x, frame.y, 
+        spriteWidth , spriteHeight, 0, 0, spriteWidth * renderScale,
+        spriteHeight * renderScale);  
 
     gameFrame++;
     requestAnimationFrame(animate);
