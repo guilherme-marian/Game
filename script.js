155f471b3a066c52f4088e214708526f8abe8b7b
Guilherme Marian <guimarian123@gmail.com>
2025-10-26 22:54:32 -0300

    plataforms

--- script.js
+++ script.js
@@ -6,22 +6,28 @@ window.addEventListener('load', function() {
     const CANVAS_WIDTH = canvas.width = 576;
     const CANVAS_HEIGHT = canvas.height = 324;
     let gameFrame = 0;
+    const TILE_WIDTH = 32;
+    const TILE_HEIGHT = 32;
+
     
     class InputHandler {
         constructor() {
-            this.keys = [];
-            this.validKeys = ['ArrowLeft', 'ArrowRight', 'z', 'x'];
+            this.keys = new Set();
+            this.validKeys = new Set(['ArrowLeft', 'ArrowRight', 'z', 'x', 'r']);
 
             window.addEventListener('keydown', e => {
-                if (this.validKeys.includes(e.key) && !this.keys.includes(e.key)) {
-                    this.keys.push(e.key);
+                if (this.validKeys.has(e.key) && !this.keys.has(e.key)) {
+                    this.keys.add(e.key);
                 }
             });
             window.addEventListener('keyup', e => {
-                if (this.validKeys.includes(e.key) > -1) {
-                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                if (this.validKeys.has(e.key)) {
+                    this.keys.delete(e.key);
                 }
             });
+        }
+        isPressed(key) {
+            return this.keys.has(key);
         }
     }
 
@@ -29,22 +35,25 @@ window.addEventListener('load', function() {
         constructor(gameWidth, gameHeight) {
             this.gameWidth = CANVAS_WIDTH;
             this.gameHeight = CANVAS_HEIGHT;
-            this.renderScale = -1.9;
+            this.renderScale = -1;
             this.width = 32;
             this.height = 32;
-            this.x = 0;
-            this.y = this.gameHeight - this.height;
+            this.x = 32;
+            this.y = 0;
             this.image = document.getElementById('playerImage');
             this.frameX = 0;
             this.frameY = 0;
             this.maxFrame = 4;
-            this.speed = 5;
-            this.rollSpeed = 10;
+            this.speed = 3;
+            this.rollSpeed = 4;
             this.isRolling = false;
             this.rollTimer = 0;
             this.vy = 0;
             this.grav = 1;
             this.facingLeft = false;
+            this.collisionOffsetY = 32;
+            this.jumpCooldown = 0;
+            this.jumpCooldownMax = 10;
 
             if(this.facingLeft) {
                 this.rollSpeed = -10;
@@ -110,6 +119,11 @@ window.addEventListener('load', function() {
             let position = Math.floor(gameFrame / this.staggerFrames) % animation.loc.length;
             const frame = animation.loc[position];
 
+            const { x: playerX, width: playerWidth } = this.getCollisionBox();
+            ctx.strokeStyle = 'blue';
+            ctx.strokeRect(playerX, this.y, playerWidth, this.height);
+
+
             ctx.save();
             if (this.facingLeft) {
                 ctx.translate(this.x, this.y);
@@ -132,7 +146,7 @@ window.addEventListener('load', function() {
 
             
         }
-        update(input) {
+        update(input, plataforms) {
 
             if(this.isRolling) {
                 this.rollTimer++;
@@ -142,26 +156,36 @@ window.addEventListener('load', function() {
 
                 if(this.rollTimer >= this.rollDuration) {
                     this.isRolling = false;
-                    this.rollTimer = 0 / this.rollDuration;
+                    this.rollTimer = 0;
                     this.currentAnimation = 'idle';
                 }
                 return;
             }
-            if (input.keys.indexOf('ArrowRight') > -1) {
+            if (this.jumpCooldown > 0) {
+                this.jumpCooldown--;
+            }
+
+            if (input.isPressed('ArrowRight')) {
                 this.x += this.speed;
                 this.currentAnimation = 'run';
                 this.facingLeft = false;
-            } else if (input.keys.indexOf('ArrowLeft') > -1) {
+            } else if (input.isPressed('ArrowLeft')) {
                 this.x -= this.speed;
                 this.currentAnimation = 'run';
                 this.facingLeft = true;
             }
-            else if (input.keys.indexOf('z') > -1 && this.OnGround()) {
+            if (input.isPressed('z') && this.OnGround(plataforms) && this.jumpCooldown === 0) {
                 this.vy -= 10;
-            } else if (input.keys.indexOf('x') > -1 && !this.isRolling && this.OnGround()) {
+                this.jumpCooldown = this.jumpCooldownMax;
+            }
+            if(
+                input.isPressed('x') &&
+                !this.isRolling &&
+                (this.OnGround(plataforms) || this.checkPlataformCollision(plataforms))
+            ) {
                 this.currentAnimation = 'roll';
                 this.isRolling = true;
-                this.rollTimer = 0;
+                this.rollTimer = 50;
             }
             else {
                 this.currentAnimation = 'idle';
@@ -180,24 +204,209 @@ window.addEventListener('load', function() {
             //vertical  
             this.y += this.vy;
 
-            if (!this.OnGround()) {
+            const landedOnPlatform = this.checkPlataformCollision(plataforms);
+
+            if (!this.OnGround() && !landedOnPlatform) {
                 this.vy += this.grav;
-                if (this.currentAnimation !== 'roll')
-                {
+                if (this.currentAnimation !== 'roll') {
                     this.currentAnimation = 'jump';
                 }
-            } else {
+            } else if (landedOnPlatform) {
                 this.vy = 0;
-                this.y = this.gameHeight - this.height;
-                
+            }
+
+            if(this.Death() < this.y) {
+                this.currentAnimation = 'death';
+                ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+                ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+                ctx.fillStyle = 'white';
+                ctx.font = '20px Arial';
+                ctx.textAlign = 'center';
+                ctx.fillText('Game Over, pressione R para tentar Novamente', CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+
+                if (input.isPressed('r')) {
+                    resetLevel();
+                }
             }
             if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
         }
-        OnGround() {
-            return this.y >= this.gameHeight - this.height;
+        OnGround(plataforms) {
+            const plataform = this.checkPlataformCollision(plataforms);
+            const playerBottom = this.y + this.height + this.collisionOffsetY;
+            return (plataform && this.vy >= 0);
+        }
+        checkPlataformCollision(plataforms) {
+            if (!Array.isArray(plataforms)) return false; 
+
+            for (let plataform of plataforms) {
+                const playerBottom = this.y + this.height + this.collisionOffsetY - this.vy;
+                const playerCurrentBottom = this.y + this.height + this.collisionOffsetY;
+
+                const onTop = playerBottom <= plataform.y &&
+                              playerCurrentBottom >= plataform.y;
+
+                const { x: playerX, width: playerWidth } = this.getCollisionBox();
+
+                const withinX = playerX + playerWidth >= plataform.x &&
+                                playerX <= plataform.x + plataform.width;
+
+                if (onTop && withinX) {
+                    this.y = plataform.y - this.height - this.collisionOffsetY;
+                    return true;
+                }
+            }
+            return false;
+        }
+        getCollisionBox() {
+            const drawWidth = this.width * Math.abs(this.renderScale);
+            const shrinkAmount = 1;
+            if (this.facingLeft) {
+                return {
+                    x: this.x - drawWidth + shrinkAmount,
+                    width: drawWidth - shrinkAmount * 2
+                };
+            } else {
+                return {
+                    x: this.x - drawWidth + shrinkAmount,
+                    width: drawWidth - shrinkAmount * 2
+                };
+            }
+        }
+        Death() {
+            return this.gameHeight - this.height;
+        }
+        
+
+    }
+function resetLevel() {
+            player.x = 32;
+            player.y = 0;
+            player.vy = 0;
+            gameFrame = 0;
+            player.currentAnimation = 'idle';
+        }
+    class Plataform {
+        constructor(x, y, gameWidth, gameHeight, currentAnimation) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = document.getElementById('plataformImage');
+            this.x = x;
+            this.y = y;
+            this.width = 16;
+            this.height = 16;
+            this.renderScale = 1.9;
+
+            //sprite rendering
+            this.columns = 1;
+            this.staggerFrames = 10;
+            this.spriteAnimations = {};
+            this.currentAnimation = currentAnimation;
+            this.animationStates = [
+                { 
+                    name: 'short_green',
+                    frames: 1, 
+                    row: 0,
+                    column: 0
+                },
+                {
+                    name: 'short_brown',
+                    frames: 1,
+                    row: 1,
+                    column: 0
+                },
+                {
+                    name: 'short_yellow',
+                    frames: 1,
+                    row: 2,
+                    column: 0
+                },
+                {
+                    name: 'short_blue',
+                    frames: 1,
+                    row: 3,
+                    column: 0
+                },
+                {
+                    name: 'wide_green',
+                    frames: 2,
+                    row: 0,
+                    column: 1
+                },
+                {
+                    name: 'wide_brown',
+                    frames: 2,
+                    row: 1,
+                    column: 1
+                },
+                {
+                    name: 'wide_yellow',
+                    frames: 2,
+                    row: 2,
+                    column: 1
+                },
+                {
+                    name: 'wide_blue',
+                    frames: 2,
+                    row: 3,
+                    column: 1
+                }
+            ];
+
+
+            this.animationStates.forEach((state) => {
+                let frames = {
+                    loc: []
+                };
+
+                const positionX = state.column * this.width;
+                const positionY = state.row * this.height;
+                const frameWidth = this.width * state.frames;
+
+                frames.loc.push({ x: positionX, y: positionY, width: frameWidth });
+
+                this.spriteAnimations[state.name] = frames;
+            });
+
+        }
+
+        draw(ctx, gameFrame) {
+            const animation = this.spriteAnimations[this.currentAnimation];
+
+            let position = Math.floor(gameFrame / this.staggerFrames) % animation.loc.length;
+            const frame = animation.loc[position];
+            ctx.strokeStyle = 'black';
+            ctx.strokeRect(this.x, this.y - 32, frame.width, this.height);
+
+            ctx.drawImage(
+                this.image,
+                frame.x, frame.y, frame.width, this.height,
+                this.x, this.y - 32, frame.width, this.height
+            );
         }
     }
-    
+
+    class Tile {
+        constructor(image, tileX, tileY, x, y, width = TILE_WIDTH, height = TILE_HEIGHT) {
+            this.image = image;       // Sprite sheet
+            this.tileX = tileX;       // Column in tileset
+            this.tileY = tileY;       // Row in tileset
+            this.x = x;               // Position on canvas
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+
+        draw(ctx) {
+            ctx.drawImage(
+                this.image,
+                this.tileX * this.width, this.tileY * this.height, this.width, this.height,
+                this.x, this.y, this.width, this.height
+            );
+        }
+    }
+
+
     class Background {
         constructor(gameWidth, gameHeight) {
             this.gameWidth = gameWidth;
@@ -223,17 +432,53 @@ window.addEventListener('load', function() {
     const backgroundImage = new Background(CANVAS_WIDTH, CANVAS_HEIGHT);
 
     const input = new InputHandler();
-    const player = new Player(CANVAS_WIDTH, CANVAS_HEIGHT);
-    player.draw(ctx, gameFrame);
-    player.update(input);
+    const player = new Player(CANVAS_WIDTH, CANVAS_HEIGHT);   
+    const tileMap = [
+        [ { x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 } ],
+        [ { x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 } ],
+        [ { x: 0, y: 2 }, { x: 1, y: 2 }, { x: 2, y: 2 } ]
+    ];
+
+    function drawTileMap(ctx, image, tileMap) {
+        for (let row = 0; row < tileMap.length; row++) {
+            for (let col = 0; col < tileMap[row].length; col++) {
+                const tile = tileMap[row][col];
+                const screenX = col * TILE_WIDTH;
+                const screenY = row * TILE_HEIGHT;
+                const t = new Tile(image, tile.x, tile.y, screenX, screenY);
+                t.draw(ctx);
+            }
+        }
+    }
 
 
+
+    const plataforms = [
+        new Plataform(0, 300, CANVAS_WIDTH, CANVAS_HEIGHT, 'wide_green'),
+        new Plataform(32, 300, CANVAS_WIDTH, CANVAS_HEIGHT, 'wide_blue'),
+        new Plataform(64, 300, CANVAS_WIDTH, CANVAS_HEIGHT, 'wide_yellow'),
+        new Plataform(96, 300, CANVAS_WIDTH, CANVAS_HEIGHT, 'wide_green'),
+        new Plataform(128, 300, CANVAS_WIDTH, CANVAS_HEIGHT, 'wide_blue'),
+        new Plataform(200, 250, CANVAS_WIDTH, CANVAS_HEIGHT, 'wide_yellow'),
+        new Plataform(250, 200, CANVAS_WIDTH, CANVAS_HEIGHT, 'wide_blue'),
+        new Plataform(300, 250, CANVAS_WIDTH, CANVAS_HEIGHT, 'wide_green'),
+        new Plataform(350, 300, CANVAS_WIDTH, CANVAS_HEIGHT, 'wide_yellow'),
+        new Plataform(400, 250, CANVAS_WIDTH, CANVAS_HEIGHT, 'wide_blue'),
+        new Plataform(450, 200, CANVAS_WIDTH, CANVAS_HEIGHT, 'short_green'),
+        new Plataform(500, 250, CANVAS_WIDTH, CANVAS_HEIGHT, 'short_yellow'),
+        new Plataform(500, 300, CANVAS_WIDTH, CANVAS_HEIGHT, 'wide_blue')
+    ]
+    player.update(input, plataforms);
     function animate() {
         ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
             backgroundImage.draw(ctx);
-            player.update(input);
+            
             player.draw(ctx, gameFrame);
 
+            plataforms.forEach(plataform => {
+                plataform.draw(ctx, gameFrame);
+            });
+            player.update(input, plataforms);
         ctx.strokeStyle = 'white';
         ctx.strokeRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         gameFrame++;
